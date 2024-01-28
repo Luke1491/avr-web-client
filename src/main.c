ff24a7ac5b076c30342568e43f71521603f397cd
luke1491 <lukaszcieplicki@gmail.com>
2024-01-28 23:07:51 +0100

    Add needed files

---
+++ src/main.c
@@ -0,0 +1,25 @@
+/* 2024 Web client AVR program
+  Author: Lukasz Cieplicki
+  Target: AVR ATmega328P
+  License: MIT
+*/
+
+# ifndef __AVR_ATmega328P__
+#define __AVR_ATmega328P__
+# endif
+
+# ifndef F_CPU
+#define F_CPU 16000000UL
+# endif
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while (1) 
+    {
+    }
+
+    return 0;
+}
